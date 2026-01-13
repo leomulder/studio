06d7a1437d2c2a50808d1fe3c743f4a6a9dd028e
Leandro Siqueira <leandro.mulder12@gmail.com>
2026-01-13 01:13:48 +0000

    diminua a imagem em 3x

--- src/components/landing/preview.tsx
+++ src/components/landing/preview.tsx
@@ -17,15 +17,17 @@ export function Preview() {
         </div>
         {previewImage && (
           <div className="mt-12 flow-root">
-            <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
-              <Image
-                src={previewImage.imageUrl}
-                alt={previewImage.description}
-                width={1200}
-                height={800}
-                className="rounded-md shadow-2xl ring-1 ring-gray-900/10"
-                data-ai-hint={previewImage.imageHint}
-              />
+            <div className="mx-auto max-w-max">
+              <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
+                <Image
+                  src={previewImage.imageUrl}
+                  alt={previewImage.description}
+                  width={400}
+                  height={267}
+                  className="rounded-md shadow-2xl ring-1 ring-gray-900/10"
+                  data-ai-hint={previewImage.imageHint}
+                />
+              </div>
             </div>
           </div>
         )}
