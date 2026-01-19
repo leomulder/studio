067c1f824adf2f2615c91070dea980fb0a53ff2e
Leandro Siqueira <leandro.mulder12@gmail.com>
2026-01-19 14:49:46 +0000

    vamos adicionar novo codigo pixel...
    import Script from "next/script";

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,21 +31,29 @@ export default function RootLayout({
   return (
     <html lang="fr" className={cn('!scroll-smooth', playfairDisplay.variable, ptSans.variable)}>
       <head>
-        <Script id="utmify-pixel-loader" strategy="afterInteractive">
-          {`
-            window.pixelId = "6968210716f60bda1549eed9";
-            var a = document.createElement("script");
-            a.setAttribute("async", "");
-            a.setAttribute("defer", "");
-            a.setAttribute("src", "https://cdn.utmify.com.br/scripts/pixel/pixel.js");
-            document.head.appendChild(a);
-          `}
-        </Script>
+        {/* UTMIFY PIXEL */}
         <Script
-            id="utmify-utms-loader"
-            src="https://cdn.utmify.com.br/scripts/utms/latest.js"
-            data-utmify-prevent-subids
-            strategy="afterInteractive"
+          id="utmify-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.pixelId = "6968210716f60bda1549eed9";
+              (function () {
+                var a = document.createElement("script");
+                a.async = true;
+                a.src = "https://cdn.utmify.com.br/scripts/pixel/pixel.js";
+                document.head.appendChild(a);
+              })();
+            `,
+          }}
+        />
+
+        {/* UTMIFY UTM HANDLER */}
+        <Script
+          id="utmify-utms"
+          strategy="afterInteractive"
+          src="https://cdn.utmify.com.br/scripts/utms/latest.js"
+          data-utmify-prevent-subids
         />
       </head>
       <body className={cn('font-body antialiased')}>
