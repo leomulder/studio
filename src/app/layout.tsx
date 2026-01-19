d39d568c37f84680878cc1a2b6be72933e3ea3ef
Leandro Siqueira <leandro.mulder12@gmail.com>
2026-01-19 16:14:34 +0000

    adicione no head codigo pixel.
    <script>
      window.pixelId = "696e57148b

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Toaster } from '@/components/ui/toaster';
 import './globals.css';
 import { cn } from '@/lib/utils';
 import { Playfair_Display, PT_Sans } from 'next/font/google';
+import Script from 'next/script';
 
 const playfairDisplay = Playfair_Display({
   subsets: ['latin'],
@@ -30,6 +31,26 @@ export default function RootLayout({
   return (
     <html lang="fr" className={cn('!scroll-smooth', playfairDisplay.variable, ptSans.variable)}>
       <head>
+        <Script
+          id="utmify-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.pixelId = "696e57148bdb78f8f4be45e1";
+              var a = document.createElement("script");
+              a.setAttribute("async", "");
+              a.setAttribute("defer", "");
+              a.setAttribute("src", "https://cdn.utmify.com.br/scripts/pixel/pixel.js");
+              document.head.appendChild(a);
+            `,
+          }}
+        />
+        <Script
+          id="utmify-utms"
+          strategy="afterInteractive"
+          src="https://cdn.utmify.com.br/scripts/utms/latest.js"
+          data-utmify-prevent-subids
+        />
       </head>
       <body className={cn('font-body antialiased')}>
         {children}
